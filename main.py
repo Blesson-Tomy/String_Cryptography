8355b8da2d1f9bd554e7028537e7b5138bb42e7d
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2024-08-04 02:02:59 +0530

    added cypher

---
+++ main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+encrypt = {
+    "a": "m",
+    "b": "n",
+    "c": "b",
+    "d": "v",
+    "e": "c",
+    "f": "x",
+    "g": "z",
+    "h": "j",
+    "i": "l",
+    "j": "h",
+    "k": "k",
+    "l": "i",
+    "m": "a",
+    "n": "b",
+    "o": "p",
+    "p": "o",
+    "q": "y",
+    "r": "f",
+    "s": "z",
+    "t": "r",
+    "u": "w",
+    "v": "d",
+    "w": "u",
+    "x": "f",
+    "y": "q",
+    "z": "s",
+}
+
+st.set_page_config(page_title="Cryptography", page_icon="🔐", layout="wide")
+st.title("Cryptography")
+st.write("This is a simple web app that allows you to encrypt and decrypt text.")
+op=st.text_area("Enter the text you would like to encrypt here:", height=200)
+
+leng=len(op)
+for i in range: leng
+    if 
+    
+    
+    
+    
+
+
+#st.text_area("Enter the text you would like to decrypt here:", height=200)
+
